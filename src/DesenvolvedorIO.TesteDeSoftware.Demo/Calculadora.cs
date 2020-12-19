bfd80a32f22e1622ebcda077a566a6454aa50ea3
renatofrancisco <renatofrancisco.s@gmail.com>
2020-12-19 16:10:31 -0300

    O básico sobre testes de unidade

---
+++ src/DesenvolvedorIO.TesteDeSoftware.Demo/Calculadora.cs
@@ -0,0 +1,9 @@
+﻿namespace DesenvolvedorIO.TesteDeSoftware.Demo
+{
+    public class Calculadora
+    {
+        public double Somar(double v1, double v2) => v1 + v2;
+
+        public double Dividir(int v1, int v2) => v1 / v2;
+    }
+}
---
+++ tests/DesenvolvedorIO.TesteDeSoftware.DemoTests/CalculadoraTests.cs
@@ -0,0 +1,38 @@
+﻿using DesenvolvedorIO.TesteDeSoftware.Demo;
+using Xunit;
+
+namespace DesenvolvedorIO.TesteDeSoftware.DemoTests
+{
+    public class CalculadoraTests
+    {
+        [Fact]
+        public void Calculadora_Somar_RetonarValorSoma()
+        {
+            // Arrange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(2, 2);
+
+            // Assert
+            Assert.Equal(4, resultado);
+        }
+
+        [Theory]
+        [InlineData(2, 2, 4)]
+        [InlineData(2, 7, 9)]
+        [InlineData(12, 4, 16)]
+        [InlineData(25, 27, 52)]
+        public void Calculadora_Somar_RetornarValoresSomaCorretos(double v1, double v2, double total)
+        {
+            // Arange
+            var calculadora = new Calculadora();
+
+            // Act
+            var resultado = calculadora.Somar(v1, v2);
+
+            // Assert
+            Assert.Equal(total, resultado);
+        }
+    }
+}
