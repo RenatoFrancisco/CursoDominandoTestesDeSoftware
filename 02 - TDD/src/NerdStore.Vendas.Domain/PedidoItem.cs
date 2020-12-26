cae4a63b0a55ed87d9772680462e5f2b29536a6b
renatofrancisco <renatofrancisco.s@gmail.com>
2020-12-26 18:25:55 -0300

    Pedido - Adicionar Item - Parte 1

--- 02 - TDD/src/NerdStore.Vendas.Domain/PedidoItem.cs
+++ 02 - TDD/src/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using NerdStore.Core.DomainObjects;
 
 namespace NerdStore.Vendas.Domain
 {
@@ -10,24 +9,15 @@ namespace NerdStore.Vendas.Domain
         public int Quantidade { get; private set; }
         public decimal ValorUnitario { get; private set; }
 
-        public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
+        public PedidoItem(Guid produtoId, 
+                          string produtoNome, 
+                          int quantidade, 
+                          decimal valorUnitario)
         {
-            if (quantidade < Pedido.MIN_UNIDADES_ITEM) throw new DomainException($"Mínimo de {Pedido.MIN_UNIDADES_ITEM} unidades por produto");
-
             ProdutoId = produtoId;
             ProdutoNome = produtoNome;
             Quantidade = quantidade;
             ValorUnitario = valorUnitario;
-        }
-
-        internal void AdicionarUnidades(int unidades)
-        {
-            Quantidade += unidades;
-        }
-
-        internal decimal CalcularValor()
-        {
-            return Quantidade * ValorUnitario;
         }
     }
 }
